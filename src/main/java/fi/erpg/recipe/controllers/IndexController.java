4cb3893dfa655d591915e4bf7b9b57faf0a347aa
Jaakko Kaisanlahti <jaakko.kaisanlahti@erpg.fi>
2019-12-15 14:50:21 +0200

    8.147 Spring Data JPA query methods.

--- src/main/java/fi/erpg/recipe/controllers/IndexController.java
+++ src/main/java/fi/erpg/recipe/controllers/IndexController.java
@@ -1,16 +1,34 @@
 package fi.erpg.recipe.controllers;
 
+import fi.erpg.recipe.domain.Category;
+import fi.erpg.recipe.domain.UnitOfMeasure;
+import fi.erpg.recipe.repositories.CategoryRepository;
+import fi.erpg.recipe.repositories.UnitOfMeasureRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 public class IndexController {
 
+    private final CategoryRepository categoryRepository;
+    private final UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
 
     @RequestMapping({"", "/", "/index"})
     public String getIndexPage() {
 
-        System.out.println("Yay");
+        Optional<Category> categoryOptional = categoryRepository.findByDescription("American");
+        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
+
+        System.out.println("Cat id is: " + categoryOptional.get().getId());
+        System.out.println("Unit of measure id is: " + unitOfMeasureOptional.get().getId());
+
         return "index";
     }
 }
