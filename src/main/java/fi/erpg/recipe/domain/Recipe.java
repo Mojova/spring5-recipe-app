451a3e090a60a9b03444feefa95f54f1ee2d2678
Jaakko Kaisanlahti <jaakko.kaisanlahti@erpg.fi>
2019-12-15 14:05:55 +0200

    8.142 JPA enumerations.

--- src/main/java/fi/erpg/recipe/domain/Recipe.java
+++ src/main/java/fi/erpg/recipe/domain/Recipe.java
@@ -2,6 +2,8 @@ package fi.erpg.recipe.domain;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -33,6 +35,9 @@ public class Recipe {
 
     @OneToOne(cascade = CascadeType.ALL)
     private Notes notes;
+
+    @Enumerated(value = EnumType.STRING)
+    private Difficulty difficulty;
 
     public Long getId() {
         return id;
@@ -120,5 +125,13 @@ public class Recipe {
 
     public void setIngredients(Set<Ingredient> ingredients) {
         this.ingredients = ingredients;
+    }
+
+    public Difficulty getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(Difficulty difficulty) {
+        this.difficulty = difficulty;
     }
 }
